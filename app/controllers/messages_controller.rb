88eb4e5ac8d01330d83d1a11197d4415c125fca2
pablopawpaw <lpault29@gmail.com>
2018-06-30 19:02:00 -0400

    basic sms get post reply done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,19 @@
 class MessagesController < ApplicationController
   def reply
     message_body = params["Body"]
-    from_number = params["From"]
+    sender_number = params["From"]
+    our_number = params["To"]
+    puts message_body
     boot_twilio
     sms = @client.messages.create(
-      from: Rails.application.secrets.twilio_number,
-      to: from_number,
-      body: "Hello there, thanks for texting me. Your number is #{from_number}."
+      from: our_number,
+      to: sender_number,
+      body: "Hello there, thanks for texting me. Your number is #{sender_number}."
     )
+  end
+
+  def show
+    #code
   end
 
   private
